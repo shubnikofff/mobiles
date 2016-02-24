10573fee4d60a8c8acac05c973b6baccb29c3227
Shubnikov Alexey <shubnikov.av@gmail.com>
2016-02-24 16:15:10 +0300

    Add console command history/update for update DB

---
+++ commands/HistoryController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * mobiles
+ * Created: 24.02.16 15:31
+ * @copyright Copyright (c) 2016 OSKR NIAEP
+ */
+
+namespace app\commands;
+use app\modules\directory\models\Employee;
+use app\modules\mobile\models\Number;
+use yii\console\Controller;
+
+
+/**
+ * @author Shubnikov Alexey <a.shubnikov@niaep.ru>
+ *
+ * HistoryController
+ */
+
+class HistoryController extends Controller
+{
+    public function actionUpdate()
+    {
+        //echo count(Number::find()->all());
+
+        foreach(Number::find()->all() as $number) {
+            $history = [];
+            foreach($number->history as $item) {
+                /** @var Employee $employee */
+                $employee = Employee::findOne(['id' => $item['ownerId']]);
+                if (!is_null($employee)) {
+                    $newItem = [
+                        'ownerName' => $employee->fullName,
+                        'ownerPost' => $employee->post,
+                    ];
+                    if (isset($item['rentDate'])) {
+                        $newItem['rentDate'] = $item['rentDate'];
+                    }
+                    if (isset($item['returnDate'])) {
+                        $newItem['returnDate'] = $item['returnDate'];
+                    }
+                    $history[] = $newItem;
+                }
+                $number->history = $history;
+                $number->save(false);
+            }
+        }
+    }
+}
